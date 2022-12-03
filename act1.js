18f3585a6455a9a64cd68fbd34f16b64eb445a8e
gerardodl <119756559+gerardodl@users.noreply.github.com>
2022-12-03 12:33:31 -0800

    Initial commit

---
+++ act1.js
@@ -0,0 +1,8 @@
+//Utilizamos var para declarar una variable como en este caso lo seria buenas, nombre, edad y edadd. 
+//En este caso despues de var se encuentra el nombre de un objeto llamado gerardo el cual guardara todas sus variables.
+var nombre = "Gerardo", apellido = "Diaz";
+var edad = 17;
+
+//En console.Los son los datos del objetos que apareceran en console
+console.log('Hola'+' '+ nombre +' '+ apellido);
+console.log("Tienes "+ edad + " años");
